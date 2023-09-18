809c6fedfcd6d80445ade865c31be237477c33f3
demba75009 <b-demba@hotmail.fr>
2023-09-19 00:54:41 +0200

    intro foot-shop

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+import {ProductList} from './feature/index';
+
+function App() {
+  return (
+    <div className="App">
+      <ProductList/>
+      
+    </div>
+  );
+}
+
+export default App;
