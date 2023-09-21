0e7009340ab752f21a299c060c465c4fbfcc5f18
demba75009 <b-demba@hotmail.fr>
2023-09-21 19:14:30 +0200

    add Product Fin

--- back/server.js
+++ back/server.js
@@ -12,6 +12,16 @@ app.use(bodyParser.json());
 
 mongoose
 
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+
+  next();
+})
+
 app.use(session({
   secret: "dffdsfd", resave:false, saveUninitialized:false, 
   cookie: {maxAge: 3600000} 
