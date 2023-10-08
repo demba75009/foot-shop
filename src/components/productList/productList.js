c3df7a77ce267b629ded9c230b0339e7ea15f4fe
demba75009 <b-demba@hotmail.fr>
2023-10-08 02:24:26 +0200

    AddProduitCartOK

--- src/components/productList/productList.js
+++ src/components/productList/productList.js
@@ -14,70 +14,109 @@ function ProductList() {
 
    const [Produits, setProduit] = useState(Prod)
    
-   console.log(Produits);
+   
+   // État pour stocker le panier
+   const [panier, setPanier] = useState(Cart);
+   
 
-    // État pour stocker le panier
-    const [panier, setPanier] = useState(Cart);
-
-    // console.log(panier);
-
-    // panier.forEach(o1 => {
-
-    //   const o2 = Produits.find(o2 =>o2[o2.InPanier] === o1[o1.produit.InPanier])
+    panier.forEach(o1 => {
 
 
-    //   if (o2 && o1.InPanier !== o2.InPanier) {
-    //     console.log("true");
-    //     console.log(o2);
-    //     // Si un objet correspondant est trouvé et que la valeur de la clé est différente, mettez à jour la valeur
-    //     o2.InPanier = true;
-    //   }
 
-    // })
+      const o2 = Produits.find(o2 =>o2._id === o1.produit._id)
+
+
+
+      if (o2 && o1.InPanier !== o2.InPanier) {
+      
+        // Si un objet correspondant est trouvé et que la valeur de la clé est différente, mettez à jour la valeur
+        o2.InPanier = true;
+      }
+
+    })
 
   
     // Fonction pour ajouter un produit au panier
     const AddPanier = (id, quantite) => {
          const produit = Produits.find(p=>p._id===id)
           
-          const nouvelArticle = { produit, quantite };
-
-          console.log(nouvelArticle);
-        
-          const tooglePanier =  panier.some(p =>p[produit.Nom] === nouvelArticle.Nom)
-          console.log(tooglePanier);
-  
-         if(tooglePanier)
-           nouvelArticle.produit.InPanier = true
-  
-  
-          setPanier([...panier, nouvelArticle]);
-
-              // Sérialisez le panier en JSON
-        const panierJSON = JSON.stringify(panier);
-
-        // Stockez la chaîne JSON dans le localStorage avec une clé (par exemple, "panier")
-        localStorage.setItem('panier', panierJSON);
-
-      
-        toast.success(`${produit.Nom} a été ajouté au panier !`);
+         
+         const nouvelArticle = { produit, quantite };
 
        
+         const tooglePanier =  panier.some(p =>p[produit.Nom] === nouvelArticle.Nom)
+ 
+        if(tooglePanier)
+          nouvelArticle.produit.InPanier = true
 
+          const nouveauPanier = [...panier];
+
+          nouveauPanier.push(nouvelArticle)
+
+         
+            setPanier(nouveauPanier);  
+  
+                // Sérialisez le panier en JSON
+          const panierJSON = JSON.stringify(nouveauPanier);
+  
+  
+          // Stockez la chaîne JSON dans le localStorage avec une clé (par exemple, "panier")
+          localStorage.setItem('panier', panierJSON);
+  
+        
+          toast.success(`${produit.Nom} a été ajouté au panier !`);
+
+       
     };
     
+    function DeletePanier (id,quantite)  {
+
+      const produit = Produits.find(p=>p._id===id)
+          
+      const nouvelArticle = { produit, quantite };
+
+    
+      const tooglePanier =  panier.some(p =>p[produit.Nom] === nouvelArticle.Nom)
+
+     if(tooglePanier)
+       nouvelArticle.produit.InPanier = false
+
+       const tableauLocalStorage = JSON.parse(localStorage.getItem('panier'));
+
+       var indexASupprimer = -1;
+
+
+       for (var i = 0; i < tableauLocalStorage.length; i++) {
+        if (tableauLocalStorage[i].produit._id === id ) {
+          indexASupprimer = i;
+          break; // Quittez la boucle une fois que l'objet est trouvé
+        }
+      }
+
+      if (indexASupprimer !== -1) {
+        tableauLocalStorage.splice(indexASupprimer, 1);
+      }
+      
+
+ 
+
+      localStorage.setItem('panier', JSON.stringify(tableauLocalStorage));
+
+      setPanier(tableauLocalStorage);
+
+
+
+  
+    toast.success(`${produit.Nom} a été retiré au panier !`);
+
+    }
+
 
    function Detail(id){
 
-
     history(`/Product/${id}`)
 
-
   }
-
-
-  
-
 
 
 
@@ -96,6 +135,7 @@ function ProductList() {
       ProductList = {p}
       ProductDetailAction={()=>Detail(p._id)}
       ProductAddPanier={()=>AddPanier(p._id,1)}
+      ProductDeletePanier={()=>DeletePanier(p._id,1)}
 
       />
 
