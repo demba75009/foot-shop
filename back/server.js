9f901657175a34827db93b71ef6a59f8b3f3af3e
demba75009 <b-demba@hotmail.fr>
2023-11-09 14:59:08 +0100

    back

--- back/server.js
+++ back/server.js
@@ -17,7 +17,7 @@ mongoose
 
 
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Origin', 'https://foot-shop-tau.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
