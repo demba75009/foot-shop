40af2696b0f88cc447655655b9eccfe2e35be28d
demba75009 <b-demba@hotmail.fr>
2023-11-09 14:48:32 +0100

    testBackEnv

--- back/database/index.js
+++ back/database/index.js
@@ -1,6 +1,10 @@
 import mongoose from "mongoose"
 
+import dotenv from 'dotenv';
 
-mongoose.connect("mongodb+srv://donkey:dembaleboss@cluster0.m3r9p.mongodb.net/Foot-Shop?retryWrites=true&w=majority").then(res=>console.log("connectez"))
+dotenv.config();
+
+
+mongoose.connect(process.env.MONGODB_URL).then(res=>console.log("connectez"))
 
 export default  mongoose
